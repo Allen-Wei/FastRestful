8abf0d4b07cbfd0181a735c70de590f5e7955db5
Allen-Wei <alan.wei@live.com>
2015-02-04 17:41:38 +0800

    update

---
+++ FastRestful/Areas/Apiv1/Apiv1AreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace FastRestful.Areas.Apiv1
+{
+    public class Apiv1AreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Apiv1";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Apiv1_default",
+                "Apiv1/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ FastRestful/Areas/Apiv1/Controllers/UtilsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+
+namespace FastRestful.Areas.Apiv1.Controllers
+{
+    public class UtilsController : Controller
+    {
+      
+        public string ReqUrl(string url)
+        {
+            Response.AddHeader("Access-Control-Allow-Origin", "*");
+            var req = WebRequest.Create(url);
+            var rep = "";
+            using (var reader = new StreamReader (req.GetResponse().GetResponseStream()))
+            {
+                rep = reader.ReadToEnd();
+            }
+            Response.ContentType = "text/html";
+            return rep;
+        }
+
+    }
+}
